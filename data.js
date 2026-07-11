f3243ed986245ec04f3f6d59ffe99881ed5f03e7
Satu <264735424+satubala@users.noreply.github.com>
2026-07-11 18:45:59 +0600

    Update data.js

--- data.js
+++ data.js
@@ -13,6 +13,7 @@ export const libraryData = [
                 chapters: [
                     { title: "অধ্যায় ১: পাঠ সূচনা 🌟", url: "/books/c1/bangla/ch1.pdf" },
                     { title: "অধ্যায় ২: আমাদের দেশ 🇧🇩", url: "/books/c1/bangla/ch2.pdf" }
+                    { title: "অধ্যায় ৩: আমাদের নদী 🇧🇩", url: "/books/c1/bangla/ch2.pdf" }
                 ]
             },
             {
